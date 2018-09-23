656f08990e3e6fbccc653e5981da7b9c6778a0c7
Khaazz <lbellec@live.com>
2018-09-23 18:28:17 +0200

    Redesign Database, entire change to rss schema

--- src/modules/Rss/models/RssSchema.js
+++ src/modules/Rss/models/RssSchema.js
@@ -5,21 +5,48 @@ const Schema = mongoose.Schema;
 
 const RssSchema = new Schema({
     /** Index */
-    ID: { type: String, required: true, index: true }, // Guild ID
+    ID: { type: String, default: '1', required: true, index: true }, // ID (formal)
 
     /**
-     * One webhook per channel
+     * All feeds available [GLOBAL]
+     * Name and Image are not null if set up by bot owner
      *
-     * webhooks: { chanID: { id: "", token: "" }, chanID: { id: "", token: "" } }
+     * Support both server feeds + global feeds.
+     *  - global has a name and an image
+     *  - server feeds are stored here so only one feed object is used
+     *
+     * Only one handler for all possible feed. (central subscription)
+     *
+     * All guild subscribed to this feed with the chan used and the role to mention (null if no mention)
+     *
+     *  [
+     *      {
+     *          url: "",
+     *          last: "",
+     *          name: null,
+     *          image: null,
+     *          guilds: {
+     *              gID: { chan: chanID, role: null/roleID/everyone }
+     *              gID: { chan: chanID, role: null/roleID/everyone }
+     *          }
+     *      }
+     *  ]
      */
-    webhooks: { type: Object, default: {} },
+    feeds: { type: Array, default: [] },
 
     /**
-     * All feeds with channel, enabled state, everyone option
+     * All guilds that subscribed to at least one feed
+     * List of chan with a webhook per guild
+     * Chan associated with the webhook obj
      *
-     * feeds: { feedName: { chan: "chanID", enabled: true, everyone: true } }
+     *  {
+     *      gID: {
+     *          chanID: { id: "", token: "" },
+     *          chanID: { id: "", token: "" }
+     *      }
+     *  }
      */
-    feeds: { type: Object, default: {} },
+    guilds: { type: Object, default: {} },
 
 }, {
     strict: false,
