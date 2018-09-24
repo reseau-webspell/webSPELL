a6bef9f43e48da37701e32b826e8b64861aaba04
Khaazz <lbellec@live.com>
2018-09-24 02:20:48 +0200

    Fix to pm2 startup script

--- scripts/start.js
+++ scripts/start.js
@@ -4,25 +4,25 @@ const pm2 = require('pm2');
 
 // Start process
 console.log('>> Starting webSpell');
-pm2.connect(function(err) {
+pm2.connect((err) => {
     if (err) {
         console.error(err);
         process.exit(2);
     }
     pm2.start({
-        script: 'stableMain.js',
-        args: [ '--color' ],
-        name: 'webSpell',
+        script: 'index.js',
+        args: ['--color'],
+        name: 'WebSpell',
         exec_mode : 'fork',
         max_memory_restart : '1G',
-        cwd: 'src/main',
+        cwd: 'src',
         error: './logs/error.err',
         output: './logs/output.log',
         pid: './logs/pid.pid',
         node_args: '-r esm',
         autorestart: true,
         wait_ready: true,
-    }, function(err) {
+    }, (err) => {
         pm2.disconnect();
         if (err) throw err;
     });
